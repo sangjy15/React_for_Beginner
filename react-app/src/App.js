69ea6e00329521f467fa5fdb66da4539d53a521f
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-07 23:08:33 +0900

    timer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,17 +4,37 @@ import {useState, useEffect} from "react";
 function App() {
     const [counter, setValue] = useState(0);
     const onClick = () => setValue((prev)=> prev + 1);
-    console.log("I run all the time");
-    const iRunOnlyOne = () => {
-        console.log("I run only once");
+    const [time, setTime] = useState(600);
+    const [min, setMin] = useState(0);
+    const [sec, setSec] = useState(0);
+    const setTimer = () => {
+        setMin(parseInt(time/60));
+            setSec(time%60);
+            console.log(min);    
+            console.log(sec);
+            setTime((prev) => prev - 1); 
+            if (time < 0) {
+                clearInterval();
+            }
+        // setInterval(function() {
+        //     setMin(parseInt(time/60));
+        //     setSec(time%60);
+        //     console.log(min);    
+        //     console.log(sec);
+        //     setTime((prev) => prev - 1); 
+        //     if (time < 0) {
+        //         clearInterval();
+        //     }
+        // }, 1000);
     }
-    useEffect(iRunOnlyOne, []);
     return ( 
         <div>
-            <h1 className={styles.title}>Off day..</h1>
-            <Button text={"Continue"}/>
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            <h1>{min} : {sec} </h1>
+            <h1 className={styles.title}>Timer</h1>
+            {/* <Button text={"Continue"}/> */}
+            {/* <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button> */}
+            <button onClick={setTimer}>Timer</button>
         </div>
     );
 }
