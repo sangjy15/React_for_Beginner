919cfb6cd9f6f79ec09b8bb8700ff692e041af1e
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-10 21:53:02 +0900

    add input

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,53 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 function App() {
     const [counter, setValue] = useState(0);
-    const onClick = () => setValue((prev)=> prev + 1);
-    const [time, setTime] = useState(600);
-    const [min, setMin] = useState(0);
-    const [sec, setSec] = useState(0);
-    const [minutes, setMinutes] = useState(2);
-    const [seconds, setSeconds] = useState(0);
-
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    console.log("i run all the time");
     useEffect(() => {
-        const countdown = setInterval(() => {
-          if (parseInt(seconds) > 0) {
-            setSeconds(parseInt(seconds) - 1);
-          }
-          if (parseInt(seconds) === 0) {
-            if (parseInt(minutes) === 0) {
-                clearInterval(countdown);
-            } else {
-              setMinutes(parseInt(minutes) - 1);
-              setSeconds(59);
-            }
-          }
-        }, 1000);
-        return () => clearInterval(countdown);
-      }, [minutes, seconds]);
-      useEffect(() => {
-        const setTimer = setInterval(() => {
-          setMin(parseInt(time/60));
-            setSec(time%60);
-            setTime((prev) => prev - 1); 
-            if (time < 0) {
-                clearInterval();
-            }
-        }, 1000);
-        return () => clearInterval(setTimer);
-      })
+      console.log("CALL THE API..");
+    }, []);
     return ( 
         <div>
-            <h1>{min} : {sec} </h1>
-            <h1 className={styles.title}>Timer</h1>
-            {/* <Button text={"Continue"}/> */}
-            {/* <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button> */}
-            {/* <button onClick={setTimer}>Timer</button> */}
-            <div>
-                {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-            </div>
+          <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>Click me</button>
         </div>
     );
 }
