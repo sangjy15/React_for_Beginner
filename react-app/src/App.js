4f1f79c8baa4d00b32c4225313ad1a60d39c25f0
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-12 22:46:12 +0900

    todo list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,42 +1,18 @@
-import {useState, useEffect} from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi~");
-    return () => console.log("bye!");
-  }, []);
-  // useEffect(function () {
-  //   console.log("hi~");
-  //   return function() {
-  //     console.log("bye!");
-  //   };
-  // }, []);
-  return <h1>Hello</h1>
-}
+import {useState} from "react";
 
 function App() {
-    // const [counter, setValue] = useState(0);
-    // const [keyword, setKeyword] = useState("");
-    // const onClick = () => setValue((prev) => prev + 1);
-    // const onChange = (event) => setKeyword(event.target.value);
-    // console.log("i run all the time");
-    // useEffect(() => {
-    //   console.log("CALL THE API..");
-    // }, []);
-    // useEffect(() => {
-    //   if (keyword !== "" && keyword.length >= 5) {
-    //     console.log("SEARCH KEYWORD", keyword);
-    //   }
-    // }, [keyword]);
-    const [showing, setShowing] = useState(false);
-    const onClickShow = () => setShowing((prev) => !prev);
+    const [toDo, setToDo] = useState("");
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+      event.preventDefault();
+      console.log(toDo);
+    }
     return ( 
         <div>
-          {/* <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-          <h1>{counter}</h1>
-          <button onClick={onClick}>Click me</button> */}
-          {showing? <Hello /> : null}
-          <button onClick={onClickShow}>{showing ? "Hide" : "Show"}</button> 
+          <form onSubmit={onSubmit}>
+            <input type="text" onChange={onChange} value={toDo} placeholder="Write your todo.." />
+            <button>Add To Do</button>
+          </form>
         </div>
     );
 }
