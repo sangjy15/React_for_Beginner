4842017487f9dea19317ee2e3420736439ad4c86
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-16 21:48:28 +0900

    coin tracker

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,31 +1,16 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-      event.preventDefault();
-      if (toDo === "") {
-        return;
-      }
-      setToDos(currentArray => [toDo, ...currentArray]);
-      setToDo("");
-    };
-    console.log(toDos);
-    return ( 
-        <div>
-          <h1>My To Dos ({toDos.length})</h1> 
-          <form onSubmit={onSubmit}>
-            <input type="text" onChange={onChange} value={toDo} placeholder="Write your todo.." />
-            <button>Add To Do</button>
-          </form>
-          <hr />
-          <ul>
-            {toDos.map((item, index) => <li key={index}>{item}</li>)}
-          </ul>
-        </div>
-    );
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    }, [])
+    return <div>
+      <h1>The Coins!</h1>
+      {loading? <strong>Loading...</strong> : null}
+    </div>;
 }
 
 export default App;
