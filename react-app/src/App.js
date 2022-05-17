f5ec909caa7fecbb3bb9d5cc38131f6e9a33f6da
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-17 22:19:36 +0900

    set Data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,10 +2,14 @@ import {useEffect, useState} from "react";
 
 function App() {
     const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([])
     useEffect(() => {
       fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
     }, [])
     return <div>
       <h1>The Coins!</h1>
