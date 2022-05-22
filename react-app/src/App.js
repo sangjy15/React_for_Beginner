e9ccb50809ab338399cab8ac3323a63343d25f8b
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-22 21:47:02 +0900

    movie

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,21 @@
 import {useEffect, useState} from "react";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [moveis, setMoveis] = useState([]);
+    useEffect(() => {
+        fetch(
+            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+        )
+        .then((response) => response.json())
+        .then((json) => {
+            setMoveis(json.data.movies);
+            setLoading(false);
+        });
+    }, [])
+    console.log(moveis)
     return <div>
-     
+        {loading ? <h1>Loading...</h1> : null}
     </div>;
 }
 
