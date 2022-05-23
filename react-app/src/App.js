37523d37e3b881ef178347c630628293eafdcd11
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-23 22:54:30 +0900

    movie list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,19 +3,35 @@ import {useEffect, useState} from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [moveis, setMoveis] = useState([]);
+    const getMovies = async() => {
+        const json = await (
+            await fetch (
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+            )
+        ).json();
+        setMoveis(json.data.movies);
+        setLoading(false);
+    }
     useEffect(() => {
-        fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-        )
-        .then((response) => response.json())
-        .then((json) => {
-            setMoveis(json.data.movies);
-            setLoading(false);
-        });
+        getMovies();
     }, [])
-    console.log(moveis)
     return <div>
-        {loading ? <h1>Loading...</h1> : null}
+        {
+            loading ? 
+            <h1>Loading...</h1>:
+            <div>{moveis.map(movie => 
+                <div key={movie.id}>
+                    <img src={movie.medium_cover_image} />}
+                    <h2>{movie.title}</h2>
+                    <p>{movie.summary}</p>
+                    <ul>
+                        {movie.genres.map(g => <li key={g}>{g}</li>)}
+                    </ul>
+                </div>
+                )
+            }
+            </div>
+        }
     </div>;
 }
 
