638519643fd432f46e1203ae079c1139e8a4c47f
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-24 22:13:40 +0900

    router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,35 +1,7 @@
-import {useEffect, useState} from "react";
-import Movie from './components/Movie';
+
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [moveis, setMoveis] = useState([]);
-    const getMovies = async() => {
-        const json = await (
-            await fetch (
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-            )
-        ).json();
-        setMoveis(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, [])
-    return <div>
-        {
-            loading ? 
-            <h1>Loading...</h1>:
-            <div>{moveis.map(movie => 
-                <Movie 
-                key={movie.id} 
-                coverImage={movie.medium_cover_image} 
-                title={movie.title} 
-                summary={movie.summary} 
-                genres={movie.genres}/>
-            )}</div>
-        }
-    </div>;
+    return null;
 }
 
 export default App;
