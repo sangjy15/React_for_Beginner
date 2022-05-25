b51e50c011f4727fc0a0d3ee02b6403c5f77afea
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-05-25 22:01:01 +0900

    router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,21 @@
-
-
+import {
+    BrowerRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-    return null;
+    return <Router>
+        <Switch>
+            <Route>
+                <Detail path ="/movie" />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>;
 }
 
 export default App;
